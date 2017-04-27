9badc6bed8d1191d3cbe67d2b2bd945b2ee55872
Sean Oh <esil@tinkl.com>
2017-04-28 06:14:47 +0900

    문서 다운로드

---
+++ download.sh
@@ -0,0 +1,143 @@
+#! /bin/bash
+curl -O http://docs.doctrine-project.org/en/latest/#index.html
+
+curl -O http://docs.doctrine-project.org/en/latest/toc.html
+
+
+mkdir reference
+cd reference
+curl -O http://docs.doctrine-project.org/en/latest/reference/faq.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/configuration.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/basic-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/association-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/inheritance-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/annotations-reference.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/xml-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/yaml-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/php-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/working-with-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/working-with-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/events.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/dql-doctrine-query-language.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/query-builder.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/native-sql.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/unitofwork.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/unitofwork-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/architecture.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/advanced-configuration.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/limitations-and-known-issues.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/tools.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/transactions-and-concurrency.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/filters.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/namingstrategy.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/improving-performance.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/caching.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/partial-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/change-tracking-policies.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/best-practices.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/metadata-drivers.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/batch-processing.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/second-level-cache.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/architecture.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/configuration.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/faq.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/basic-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/association-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/inheritance-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/working-with-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/working-with-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/events.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/unitofwork.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/unitofwork-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/transactions-and-concurrency.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/batch-processing.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/dql-doctrine-query-language.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/query-builder.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/native-sql.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/change-tracking-policies.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/partial-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/xml-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/yaml-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/annotations-reference.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/php-mapping.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/caching.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/improving-performance.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/tools.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/metadata-drivers.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/best-practices.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/limitations-and-known-issues.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/filters.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/namingstrategy.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/advanced-configuration.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/second-level-cache.html
+curl -O http://docs.doctrine-project.org/en/latest/reference/security.html
+
+cd ..
+mkdir tutorials
+cd tutorials
+
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/getting-started.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/working-with-indexed-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/extra-lazy-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/composite-primary-keys.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/ordered-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/pagination.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/override-field-association-mappings-in-subclasses.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/embeddables.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/getting-started.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/getting-started-database.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/getting-started-models.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/working-with-indexed-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/extra-lazy-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/composite-primary-keys.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/ordered-associations.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/override-field-association-mappings-in-subclasses.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/pagination.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/embeddables.html
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/embeddables.html#initializing-embeddables
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/embeddables.html#column-prefixing
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/embeddables.html#dql
+curl -O http://docs.doctrine-project.org/en/latest/tutorials/pagination.html
+
+cd ..
+mkdir cookbook
+cd cookbook
+
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/aggregate-fields.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/decorator-pattern.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/strategy-cookbook-introduction.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/dql-custom-walkers.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/dql-user-defined-functions.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-arrayaccess-for-domain-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-the-notify-changetracking-policy.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-wakeup-or-clone.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/working-with-datetime.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/validation-of-entities.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/entities-in-session.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/resolve-target-entity-listener.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/integrating-with-codeigniter.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/sql-table-prefixes.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/mysql-enums.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/advanced-field-value-conversion-using-custom-mapping-types.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/aggregate-fields.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/custom-mapping-types.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/decorator-pattern.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/dql-custom-walkers.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/dql-user-defined-functions.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-arrayaccess-for-domain-objects.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-the-notify-changetracking-policy.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/implementing-wakeup-or-clone.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/integrating-with-codeigniter.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/resolve-target-entity-listener.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/sql-table-prefixes.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/strategy-cookbook-introduction.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/validation-of-entities.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/working-with-datetime.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/mysql-enums.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/advanced-field-value-conversion-using-custom-mapping-types.html
+curl -O http://docs.doctrine-project.org/en/latest/cookbook/entities-in-session.html
+
+cd ..
+mkdir changelog
+cd changelog
+curl -O http://docs.doctrine-project.org/en/latest/changelog/migration_2_5.html
---
+++ src/readthedocs-data.js
@@ -0,0 +1,30 @@
+  var READTHEDOCS_DATA = {
+    project: "doctrine-orm",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/",
+    theme: "doctrine",
+    builder: "sphinx",
+    docroot: "/docs/en/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "03972c9c"
+  };
+  
+  READTHEDOCS_DATA.subprojects["doctrine-common"] = "http://docs.doctrine-project.org/projects/doctrine-common/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-mongodb-odm"] = "http://docs.doctrine-project.org/projects/doctrine-mongodb-odm/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-couchdb"] = "http://docs.doctrine-project.org/projects/doctrine-couchdb/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-migrations"] = "http://docs.doctrine-project.org/projects/doctrine-migrations/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-phpcr-odm"] = "http://docs.doctrine-project.org/projects/doctrine-phpcr-odm/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-orm"] = "http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine1"] = "http://docs.doctrine-project.org/projects/doctrine1/en/latest/";
+  
+  READTHEDOCS_DATA.subprojects["doctrine-dbal"] = "http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/";
+  
